9b6ce9386d218073efe8a04193ee6c351be43448
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2026-03-03 10:33:05 +0900

    feat: 챗봇 라우팅 추가

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
+import ChatbotPopupPage from "../pages/ChatbotPopupPage";
 import { ROUTES } from "./routes";
 
 export default function App() {
+  const layoutRoutes = ROUTES.filter((r) => r.path !== "/chatbot");
+
   return (
     <BrowserRouter>
-      <MainLayout>
-        <Routes>
-          {ROUTES.map((r) => (
+      <Routes>
+        <Route path="/chatbot" element={<ChatbotPopupPage />} />
+
+        <Route element={<MainLayout />}>
+          {layoutRoutes.map((r) => (
             <Route key={r.path} path={r.path} element={r.element} />
           ))}
-        </Routes>
-      </MainLayout>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
