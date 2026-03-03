0b67fab764d2de3ceafecec1b83cfa50782971c3
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2026-03-03 10:33:46 +0900

    feat: 레이아웃 구조 변경

--- src/components/layout/MainLayout.js
+++ src/components/layout/MainLayout.js
@@ -1,12 +1,17 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ChatbotLauncher from "../chatbot/ChatbotLauncher";
 
-export default function MainLayout({ children }) {
+export default function MainLayout() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
-      <main>{children}</main>
+      <main>
+        <Outlet />
+      </main>
       <Footer />
+      {/*<ChatbotLauncher />*/}
     </div>
   );
 }
